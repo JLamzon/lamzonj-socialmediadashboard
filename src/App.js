825157abab2e2059bcb62af3cf2ea5793ea5440d
JLamzon <jlamzon@yahoo.com>
2023-04-23 23:38:44 -0700

    update padding and width

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// Review: Jessie's  app is really neat and well coded, Him and I even shared idea because we did our project in different ways, the collaboration helped me a lot. His responsiveness is on point, Color scheme fixing and it's near just like the model we're inspired from. Goodjob overall, fix the position of the the box containers . Good job!
+
+
+
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
